8ce81eadff1aac342f92d1b90126cacf1a17e4fe
nurmanaman <mnurman@gmail.com>
2023-12-09 04:18:42 +0000

    edit sonar-properties

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const logger = require('./middleware/logger')
 app.use(logger)
 const connection = require('./middleware/db_connect');
-
+app.
 // Dashboard
 app.use('/', express.static(path.join(__dirname, 'public')));
 
